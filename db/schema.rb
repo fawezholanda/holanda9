1828f37f1e3ac531516b1d5eb9c1e22afc807c33
fawezholanda <fawezholanda@hotmail.com>
2018-10-26 15:01:34 +0000

    lesson controller created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181025220208) do
+ActiveRecord::Schema.define(version: 20181026142702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lessons", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "where"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
